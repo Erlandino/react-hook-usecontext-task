ae2e154ba83c6d7d796186f8f155cb9d37269337
Erlandino <erlend.kodehode@gmail.com>
2022-11-23 13:46:49 +0100

    added some styling, made a button for future expansion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useEffect } from "react";
-import { useContext, createContext, useState } from "react";
+// imports
+import { useContext, createContext, useState, useEffect } from "react";
 
+// createcontext variable
 const CompData = createContext();
 
+// Main component, exports to index.js
 export default function App() {
+  // useState declaration
   const [pokemon, setPokemon] = useState(null);
+
+  // Api call, useEffect together with array dependency prevents infinite loop with fetch (memory leak).
   useEffect(() => {
     async function apiCall(callback) {
       const fetchApi = await fetch("https://pokeapi.co/api/v2/pokemon/9/");
@@ -17,19 +22,15 @@ export default function App() {
   }, []);
 
   return (
+    // CompData.Provider is for useContext, allows us to send values to
+    // deeply nested components without having to use props in each component
     <CompData.Provider value={pokemon}>
-      <Comp1 />
+      <Comp2 />
     </CompData.Provider>
   );
 }
 
-function Comp1() {
-  return (
-    <>
-      <Comp2 />
-    </>
-  );
-}
+//  Component 2
 function Comp2() {
   return (
     <>
@@ -37,23 +38,47 @@ function Comp2() {
     </>
   );
 }
+
+// Component 3
 function Comp3() {
+  return (
+    <>
+      <Comp4 />
+    </>
+  );
+}
+
+// Component 4
+// deeply nested to app trough component 2 and 3
+function Comp4() {
   const pokemon = useContext(CompData);
 
   return (
-    <>
-      <h1>{pokemon && pokemon.name}</h1>
+    <section>
+      <div className="pokemons">
+        {/* only displays when pokemon is true/not empty */}
+        <h1>{pokemon && pokemon.name.toUpperCase()}</h1>
+        <img src={pokemon && pokemon.sprites.front_default} alt="" />
 
-      {pokemon &&
-        pokemon.stats.map(function (element, index) {
-          return (
-            <div key={index}>
-              <h3>{element.stat.name}</h3>
-              <p>stat: {element.base_stat}</p>
-              <p>effort: {element.effort}</p>
-            </div>
-          );
-        })}
-    </>
+        {/* 
+        Only loops trough pokemon with map when pokemon is "true"/not empty 
+        map loop function creates new html elements for each pokemon property and puts them in a container
+      */}
+        {pokemon &&
+          pokemon.stats.map(function (element, index) {
+            return (
+              <div key={index} className="pokemons__stats">
+                <h3 className="pokemons__stats__name">{element.stat.name}: </h3>
+                <p className="pokemons__stats__value">{element.base_stat}</p>
+              </div>
+            );
+          })}
+      </div>
+
+      <button className="pokemons__moreInfo">
+        Click for more info
+        <br />v
+      </button>
+    </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// imports
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// creates div with id "root", all elements within our react project will be inside this and further children
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // loads react twice to catch some bugs
   <React.StrictMode>
     <App />
   </React.StrictMode>
