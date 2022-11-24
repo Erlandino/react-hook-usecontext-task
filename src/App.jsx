f9fa4eddc4c75381adb870212666376d27d2baba
Erlandino <erlend.kodehode@gmail.com>
2022-11-24 11:28:56 +0100

    Adding a pokemon list to existing project

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,46 @@ const CompData = createContext();
 // Main component, exports to index.js
 export default function App() {
   // useState declaration
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemonList, setPokemonList] = useState(null);
+  const [pokemonOffset, setPokemonOffset] = useState(0);
 
-  // Api call, useEffect together with array dependency prevents infinite loop with fetch (memory leak).
+  console.log(pokemonOffset);
+
+  // Api call
+  async function apiCall(callback, address) {
+    const fetchApi = await fetch(address);
+    const data = await fetchApi.json();
+
+    callback(data);
+  }
+
+  function pokemons(data) {
+    setPokemonList((prevList) => (prevList ? [...prevList, data] : [data]));
+  }
+  console.log(pokemonList);
+
+  function pokemonLinksCreator(param) {
+    if (param === "+") setPokemonOffset((prevOffset) => (prevOffset === 1134 ? 0 : prevOffset + 10));
+    if (param === "-") setPokemonOffset((prevOffset) => (prevOffset === 0 ? 1134 : prevOffset - 10));
+  }
+
+  // useEffect together with array dependency prevents infinite loop with fetch (memory leak).
   useEffect(() => {
-    async function apiCall(callback, adress) {
-      const fetchApi = await fetch(adress);
-      const data = await fetchApi.json();
+    apiCall(pokemonList, `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${pokemonOffset}`);
 
-      callback(data);
+    function pokemonList(data) {
+      data.results.forEach((element) => {
+        apiCall(pokemons, element.url);
+      });
     }
-
-    apiCall(setPokemon, "https://pokeapi.co/api/v2/pokemon/9/");
-  }, []);
+  }, [pokemonOffset]);
 
   return (
     // CompData.Provider is for useContext, allows us to send values to
     // deeply nested components without having to use props in each component
-    <CompData.Provider value={pokemon}>
+    <CompData.Provider value={pokemonList}>
+      <button onClick={() => pokemonLinksCreator("-")}>Previous page</button>
+      <button onClick={() => pokemonLinksCreator("+")}>Next page</button>
       <Comp2 />
     </CompData.Provider>
   );
@@ -51,34 +73,39 @@ function Comp3() {
 // Component 4
 // deeply nested to app trough component 2 and 3
 function Comp4() {
-  const pokemon = useContext(CompData);
+  const pokemonList = useContext(CompData);
 
   return (
     <section>
-      <div className="pokemons">
-        {/* only displays when pokemon is true/not empty */}
-        <h1>{pokemon && pokemon.name.toUpperCase()}</h1>
-        <img src={pokemon && pokemon.sprites.front_default} alt="" />
-
-        {/* 
-        Only loops trough pokemon with map when pokemon is "true"/not empty 
-        map loop function creates new html elements for each pokemon property and puts them in a container
-      */}
-        {pokemon &&
-          pokemon.stats.map(function (element, index) {
-            return (
-              <div key={index} className="pokemons__stats">
-                <h3 className="pokemons__stats__name">{element.stat.name}: </h3>
-                <p className="pokemons__stats__value">{element.base_stat}</p>
+      {pokemonList && pokemonList.length >= 20
+        ? pokemonList.forEach((pokemon) => {
+            <div>
+              <div className="pokemons">
+                {/* only displays when pokemon is true/not empty */}
+                <h1>{pokemon && pokemon.name.toUpperCase()}</h1>
+                <img src={pokemon && pokemon.sprites.front_default} alt="" />
+                {/*
+          Only loops trough pokemon with map when pokemon is "true"/not empty
+          map loop function creates new html elements for each pokemon property and puts them in a container
+                */}
+                {pokemon &&
+                  pokemon.stats.map(function (element, index) {
+                    return (
+                      <div key={index} className="pokemons__stats">
+                        <h3 className="pokemons__stats__name">{element.stat.name}: </h3>
+                        <p className="pokemons__stats__value">{element.base_stat}</p>
+                      </div>
+                    );
+                  })}
               </div>
-            );
-          })}
-      </div>
 
-      <button className="pokemons__moreInfo">
-        Click for more info
-        <br />v
-      </button>
+              <button className="pokemons__moreInfo">
+                Click for more info
+                <br />v
+              </button>
+            </div>;
+          })
+        : ""}
     </section>
   );
 }
